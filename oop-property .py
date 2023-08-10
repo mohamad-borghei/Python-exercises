708a8d225da3aad8819760cdfe2781a5b29b8c06
mohamad-borghei <mohamad.b76.mb@gmail.com>
2023-08-10 15:09:26 +0330

    update 1

--- oop-property .py
+++ oop-property .py
@@ -2,26 +2,3 @@ class person:
     def __init__(self, name):
         self.__name=name
 
-    @property
-    def name(self):
-        #getter
-        print('getting name')
-        return self.__name
-    
-    @name.setter
-    def name(self,value):
-        #setter
-        print(f'setting name to {value}')
-        self.__name = value
-
-    @name.deleter
-    def name(self):
-        print('deleting name')
-        del self.__name
-
-p=person('mohamad')
-print(f'the name is {p.name}')
-print()
-p.name = 'sara'
-print(f'the name is {p.name}')
-del p.name
